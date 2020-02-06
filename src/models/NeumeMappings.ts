63b00b73b4247c781a8f1ca3cb69c2e3198d75bc
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2020-02-05 19:37:49 -0600

    Add more neumes

--- src/models/NeumeMappings.ts
+++ src/models/NeumeMappings.ts
@@ -1,6 +1,6 @@
-import { Neume, TimeNeume, QuantitativeNeume, VocalExpressionNeume, RootSign, Note } from '@/models/Neumes';
+import { Neume, TimeNeume, QuantitativeNeume, Fthora, TempoSign, VocalExpressionNeume, RootSign, Note } from '@/models/Neumes';
 
-export type NeumeFont = 'Psaltica' | 'Omega' | 'EzSpecial2';
+export type NeumeFont = 'Psaltica' | 'Omega' | 'EzSpecial1' | 'EzSpecial2' | 'EzFthora';
 
 export interface NeumeMapping {
     fontFamily: NeumeFont,
@@ -56,8 +56,116 @@ export const neumeMap = new Map<Neume, NeumeMapping>([
     [QuantitativeNeume.OligonPlusKentima, { fontFamily: 'Psaltica', text: '1~'}],
     [QuantitativeNeume.Kentemata, { fontFamily: 'Psaltica', text: '`'}],
 
-    [TimeNeume.Gorgon, { fontFamily: 'Psaltica', text: 's' }],
-    [TimeNeume.Klasma, { fontFamily: 'Psaltica', text: 'a' }],
+    [TimeNeume.Gorgon_Top, { fontFamily: 'Psaltica', text: 's' }],
+    [TimeNeume.Gorgon_Bottom, { fontFamily: 'Psaltica', text: 'x' }],
+
+    [TimeNeume.Digorgon, { fontFamily: 'Psaltica', text: 'd' }],
+    [TimeNeume.Trigorgon, { fontFamily: 'Psaltica', text: 'f' }],
+
+    [TimeNeume.GorgonDottedLeft, { fontFamily: 'Psaltica', text: 'h' }],
+    [TimeNeume.GorgonDottedRight, { fontFamily: 'EzSpecial1', text: 'h' }],
+
+    [TimeNeume.Klasma_Top, { fontFamily: 'Psaltica', text: 'a' }],
+    [TimeNeume.Klasma_Bottom, { fontFamily: 'Psaltica', text: 'z' }],
+
+    [TimeNeume.Hapli, { fontFamily: 'Psaltica', text: ';' }],
+    [TimeNeume.Dipli, { fontFamily: 'Psaltica', text: 'k' }],
+    [TimeNeume.Tripli, { fontFamily: 'EzSpecial1', text: ';' }],
+
+    [TimeNeume.Argon, { fontFamily: 'Psaltica', text: 'g' }],
+    [TimeNeume.Hemiolion, { fontFamily: 'Psaltica', text: 'G' }],
+    [TimeNeume.Diargon, { fontFamily: 'EzSpecial1', text: 'J' }],
+
+    [Fthora.DiatonicNiLow_TopCenter, { fontFamily: 'EzFthora', text: 'd' }],
+    [Fthora.DiatonicPa_TopCenter, { fontFamily: 'EzFthora', text: 'f' }],
+    [Fthora.DiatonicVou_TopCenter, { fontFamily: 'EzFthora', text: 'g' }],
+    [Fthora.DiatonicGa_TopCenter, { fontFamily: 'EzFthora', text: 'h' }],
+    [Fthora.DiatonicThi_TopCenter, { fontFamily: 'EzFthora', text: 'j' }],
+    [Fthora.DiatonicKe_TopCenter, { fontFamily: 'EzFthora', text: 'k' }],
+    [Fthora.DiatonicZo_TopCenter, { fontFamily: 'EzFthora', text: 'l' }],
+    [Fthora.DiatonicNiHigh_TopCenter, { fontFamily: 'EzFthora', text: '7' }],
+    [Fthora.HardChromaticPa_TopCenter, { fontFamily: 'EzFthora', text: '1' }],
+    [Fthora.HardChromaticThi_TopCenter, { fontFamily: 'EzFthora', text: '6' }],
+    [Fthora.SoftChromaticPa_TopCenter, { fontFamily: 'EzFthora', text: '4' }],
+    [Fthora.SoftChromaticThi_TopCenter, { fontFamily: 'EzFthora', text: '2' }],
+    [Fthora.Enharmonic_TopCenter, { fontFamily: 'EzFthora', text: '0' }],
+    [Fthora.Zygos_TopCenter, { fontFamily: 'EzFthora', text: '8' }],
+    [Fthora.Kliton_TopCenter, { fontFamily: 'EzFthora', text: '9' }],
+    [Fthora.Spathi_TopCenter, { fontFamily: 'EzFthora', text: '`' }],
+
+    [Fthora.DiatonicNiLow_TopRight, { fontFamily: 'EzFthora', text: 'D' }],
+    [Fthora.DiatonicPa_TopRight, { fontFamily: 'EzFthora', text: 'F' }],
+    [Fthora.DiatonicVou_TopRight, { fontFamily: 'EzFthora', text: 'G' }],
+    [Fthora.DiatonicGa_TopRight, { fontFamily: 'EzFthora', text: 'H' }],
+    [Fthora.DiatonicThi_TopRight, { fontFamily: 'EzFthora', text: 'J' }],
+    [Fthora.DiatonicKe_TopRight, { fontFamily: 'EzFthora', text: 'K' }],
+    [Fthora.DiatonicZo_TopRight, { fontFamily: 'EzFthora', text: 'L' }],
+    [Fthora.DiatonicNiHigh_TopRight, { fontFamily: 'EzFthora', text: '&' }],
+    [Fthora.HardChromaticPa_TopRight, { fontFamily: 'EzFthora', text: '!' }],
+    [Fthora.HardChromaticThi_TopRight, { fontFamily: 'EzFthora', text: '^' }],
+    [Fthora.SoftChromaticPa_TopRight, { fontFamily: 'EzFthora', text: '$' }],
+    [Fthora.SoftChromaticThi_TopRight, { fontFamily: 'EzFthora', text: '@' }],
+    [Fthora.Enharmonic_TopRight, { fontFamily: 'EzFthora', text: ')' }],
+    [Fthora.Zygos_TopRight, { fontFamily: 'EzFthora', text: '*' }],
+    [Fthora.Kliton_TopRight, { fontFamily: 'EzFthora', text: '(' }],
+    [Fthora.Spathi_TopRight, { fontFamily: 'EzFthora', text: '~' }],
+
+    [Fthora.DiatonicNiLow_BottomCenter, { fontFamily: 'EzFthora', text: 'z' }],
+    [Fthora.DiatonicPa_BottomCenter, { fontFamily: 'EzFthora', text: 'a' }],
+    [Fthora.DiatonicThi_BottomCenter, { fontFamily: 'EzFthora', text: 's' }],
+    [Fthora.DiatonicKe_BottomCenter, { fontFamily: 'EzFthora', text: 'x' }],
+    [Fthora.DiatonicNiHigh_BottomCenter, { fontFamily: 'EzFthora', text: 'u' }],
+    [Fthora.HardChromaticPa_BottomCenter, { fontFamily: 'EzFthora', text: 'q' }],
+    [Fthora.HardChromaticThi_BottomCenter, { fontFamily: 'EzFthora', text: 'y' }],
+    [Fthora.SoftChromaticPa_BottomCenter, { fontFamily: 'EzFthora', text: 'r' }],
+    [Fthora.SoftChromaticThi_BottomCenter, { fontFamily: 'EzFthora', text: 'w' }],
+    [Fthora.Enharmonic_BottomCenter, { fontFamily: 'EzFthora', text: 'p' }],
+    [Fthora.Zygos_BottomCenter, { fontFamily: 'EzFthora', text: 'i' }],
+    [Fthora.Kliton_BottomCenter, { fontFamily: 'EzFthora', text: 'o' }],
+
+    [Fthora.DiatonicNiLow_BottomRight, { fontFamily: 'EzFthora', text: 'Z' }],
+    [Fthora.DiatonicPa_BottomRight, { fontFamily: 'EzFthora', text: 'A' }],
+    [Fthora.DiatonicThi_BottomRight, { fontFamily: 'EzFthora', text: 'S' }],
+    [Fthora.DiatonicKe_BottomRight, { fontFamily: 'EzFthora', text: 'X' }],
+    [Fthora.DiatonicNiHigh_BottomRight, { fontFamily: 'EzFthora', text: 'U' }],
+    [Fthora.HardChromaticPa_BottomRight, { fontFamily: 'EzFthora', text: 'Q' }],
+    [Fthora.HardChromaticThi_BottomRight, { fontFamily: 'EzFthora', text: 'Y' }],
+    [Fthora.SoftChromaticPa_BottomRight, { fontFamily: 'EzFthora', text: 'R' }],
+    [Fthora.SoftChromaticThi_BottomRight, { fontFamily: 'EzFthora', text: 'W' }],
+    [Fthora.Enharmonic_BottomRight , { fontFamily: 'EzFthora', text: 'P' }],
+    [Fthora.Zygos_BottomRight, { fontFamily: 'EzFthora', text: 'I' }],
+    [Fthora.Kliton_BottomRight, { fontFamily: 'EzFthora', text: 'O' }],
+
+    [Fthora.Sharp_2_Right, { fontFamily: 'EzFthora', text: '+' }],
+    [Fthora.Sharp_2_Left, { fontFamily: 'EzFthora', text: '=' }],
+    [Fthora.Sharp_4_Right, { fontFamily: 'EzFthora', text: '}' }],
+    [Fthora.Sharp_4_Left, { fontFamily: 'EzFthora', text: ']' }],
+    [Fthora.Sharp_6_Right, { fontFamily: 'EzFthora', text: ':' }],
+    [Fthora.Sharp_6_Left, { fontFamily: 'EzFthora', text: ';' }],
+
+    [Fthora.Flat_2_Right, { fontFamily: 'EzFthora', text: '_' }],
+    [Fthora.Flat_2_Left, { fontFamily: 'EzFthora', text: '-' }],
+    [Fthora.Flat_4_Right, { fontFamily: 'EzFthora', text: '{' }],
+    [Fthora.Flat_4_Left, { fontFamily: 'EzFthora', text: '[' }],
+    [Fthora.Flat_6_Right, { fontFamily: 'EzFthora', text: '"' }],
+    [Fthora.Flat_6_Left, { fontFamily: 'EzFthora', text: '\'' }],
+
+    [Fthora.GeneralSharp_TopCenter, { fontFamily: 'EzFthora', text: '3' }],
+    [Fthora.GeneralSharp_TopRight, { fontFamily: 'EzFthora', text: '#' }],
+    [Fthora.GeneralSharp_BottomCenter, { fontFamily: 'EzFthora', text: 'e' }],
+    [Fthora.GeneralSharp_BottomRight, { fontFamily: 'EzFthora', text: 'E' }],
+    
+    [Fthora.GeneralFlat_TopCenter, { fontFamily: 'EzFthora', text: '5' }],
+    [Fthora.GeneralFlat_TopRight, { fontFamily: 'EzFthora', text: '%' }],
+    [Fthora.GeneralFlat_BottomCenter, { fontFamily: 'EzFthora', text: 't' }],
+    [Fthora.GeneralFlat_BottomRight, { fontFamily: 'EzFthora', text: 'T' }],
+
+    [TempoSign.VerySlow, { fontFamily: 'EzSpecial1', text: 'S' }],
+    [TempoSign.Slow, { fontFamily: 'EzSpecial1', text: 'a' }],
+    [TempoSign.Medium, { fontFamily: 'EzSpecial1', text: 's' }],
+    [TempoSign.Moderate, { fontFamily: 'EzSpecial1', text: 'z' }],
+    [TempoSign.Quick, { fontFamily: 'EzSpecial1', text: 'x' }],
+    [TempoSign.VeryQuick, { fontFamily: 'EzSpecial1', text: 'X' }],
 
     [VocalExpressionNeume.Vareia, { fontFamily: 'Psaltica', text: '\\'}],
     [VocalExpressionNeume.HomalonConnecting, { fontFamily: 'Psaltica', text: '['}],
@@ -125,8 +233,8 @@ const quantitativeNeumeKeyboardMap_Shift = new Map<string, QuantitativeNeume>([
 ]);
 
 const timeNeumeKeyboardMap = new Map<string, TimeNeume>([
-    ["Semicolon", TimeNeume.Gorgon],
-    ["Comma", TimeNeume.Klasma],
+    ["Semicolon", TimeNeume.Gorgon_Top],
+    ["Comma", TimeNeume.Klasma_Top],
 ]);
 
 export const KeyboardMap = {

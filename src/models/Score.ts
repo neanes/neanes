052ce6f87947bd2edd5a1ddc4337f043087dfd77
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2020-09-30 16:42:05 -0500

    Add page and line breaks

---
+++ src/models/Score.ts
@@ -0,0 +1,7 @@
+import { Element } from '@/models/Element';
+
+export class Score {
+    public elements: Element[] = [];
+    public pageBreaks: number[] = [];
+    public lineBreaks: number[] =[];
+}

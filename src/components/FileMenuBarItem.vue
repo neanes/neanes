63a1a2247f67d406e0eaef661286c5afcfefad14
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2020-09-30 20:55:33 -0500

    Add file menu and store

---
+++ src/components/FileMenuBarItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="file-menu-bar-item">
+    {{label}}
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class FileMenuBarItem extends Vue {
+  @Prop() label!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

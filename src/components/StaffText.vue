4f34313fd8872f717d331b64d8521108d08f9859
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2020-10-04 13:36:03 -0500

    Update staff text

--- src/components/StaffText.vue
+++ src/components/StaffText.vue
@@ -1,23 +1,26 @@
 <template>
-  <span 
-    class="staff-text" 
-    :style="style"
-    @dblclick="handleDoubleClick"
-    @mousedown="handleMouseDown">
+  <div class="staff-text-container">
       <ContentEditable 
-      class="staff-text-content"
-      ref="text"
-      :content="element.text" 
-      @blur="updateText($event)"></ContentEditable>
-</span>
+        ref="text"
+        class="staff-text" 
+        :style="style"
+        :content="element.text" 
+        :class="[{ selected: element == selectedElement }]"
+        :editable="editable"
+        @dblclick.native="handleDoubleClick"
+        @mousedown.native="handleMouseDown"
+        @blur="updateText($event)"></ContentEditable>
+      </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { Neume as NeumeType } from '@/models/Neumes';
+import { ScoreElement } from '@/models/Element';
 import { neumeMap } from '@/models/NeumeMappings';
 import { StaffTextElement } from '@/models/Element';
 import ContentEditable from '@/components/ContentEditable.vue';
+import { store } from '@/store';
 
 @Component({
   components: {
@@ -31,6 +34,14 @@ export default class StaffText extends Vue {
 
   startX: number = 0;
   startY: number = 0;
+
+  get selectedElement() {
+    return store.state.selectedElement;
+  }
+
+  set selectedElement(element: ScoreElement | null) {
+    store.mutations.setSelectedElement(element);
+  }
 
   get textElement() {
     return this.$refs.text as ContentEditable;
@@ -51,9 +62,7 @@ export default class StaffText extends Vue {
 
   handleDoubleClick() {
     this.editable = true;
-    this.textElement.focus();
-    
-    document.addEventListener('click', this.handleClick);
+    Vue.nextTick(this.textElement.focus)
   }
 
   handleClick(e: MouseEvent) {
@@ -61,11 +70,29 @@ export default class StaffText extends Vue {
 
     if (container !== e.target && !container.contains(e.target as Element)) {    
       this.editable = false;
+
+      // Clear any text selection that the user may have made
+      window.getSelection()!.removeAllRanges();
+
+      // If the user didn't click another element, then
+      // clear the selected element
+      // TODO do we really want to clear the selected element?
+      Vue.nextTick(() => {
+        if (this.selectedElement === this.element) {
+              this.selectedElement = null;
+        }
+      })
+      
       document.removeEventListener('click', this.handleClick);
     }
   }
 
   handleMouseDown(e: MouseEvent) {
+    if (this.selectedElement !== this.element) {
+      this.selectedElement = this.element;
+      document.addEventListener('click', this.handleClick);
+    }
+
     if (this.editable) {
       return;
     }
@@ -93,6 +120,10 @@ export default class StaffText extends Vue {
 
   updateText(text: string) {
     this.element.text = text;
+
+    if (text.trim() === '') {
+      store.mutations.removeElement(this.element);
+    }
   }
 }
 </script>
@@ -101,13 +132,30 @@ export default class StaffText extends Vue {
   .staff-text {
     position: relative;
     min-width: 10px;
-    height: 100%;
-    border: 1px dotted black;
+    border: 1px dotted transparent;
   }
 
-  .staff-text-content {
+  .staff-text.selected {
+    border: 1px dotted rgb(8, 8, 8);
+  }
+
+  .staff-text-container {
+    position: relative;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    white-space: nowrap;
+
+    width: 0;
+    max-width: 0;
+
+    top: -20px;
+  }
+
+  /* .staff-text-content {
     width: 100%;
     height: 100%;
     display: block;
-  }
+  } */
 </style>

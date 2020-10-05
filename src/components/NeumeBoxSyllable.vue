60c5511304fc51fc6ecba9ceff1f9923debbe149
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2020-10-05 09:16:57 -0500

    Add groundwork for note neume properties

--- src/components/NeumeBoxSyllable.vue
+++ src/components/NeumeBoxSyllable.vue
@@ -1,89 +1,73 @@
 <template>
   <div class="neume">
     <Neume 
-      v-if="hasVocalExpressionNeume && isVareia(neume.vocalExpressionNeume)" :neume="neume.vocalExpressionNeume"></Neume>
-    <Neume :neume="neume.quantitativeNeume"></Neume>
-    <Neume 
+      v-if="hasVocalExpressionNeume && isVareia(note.vocalExpressionNeume.neume)" :neume="note.vocalExpressionNeume.neume"></Neume>
+    <Neume :neume="note.quantitativeNeume.neume"></Neume>
+    <TimeNeumeBox
       v-if="hasTimeNeume"
-      :neume="neume.timeNeume"
+      :element="note.timeNeume"
       :style="timeNeumeStyle"
       :class="[ 
-        { high: isHighNeume(neume.quantitativeNeume) }, 
-        { red: isRedNeume(neume.timeNeume) } ]"></Neume>
+        { high: isHighNeume(note.quantitativeNeume.neume) }, 
+        { red: isRedNeume(note.timeNeume.neume) } ]"></TimeNeumeBox>
     <Neume 
       v-if="hasFthora"
-      :neume="neume.fthora"
+      :neume="note.fthora.neume"
       class="red"></Neume>
     <Neume 
-      v-if="hasVocalExpressionNeume && !isVareia(neume.vocalExpressionNeume)"
-      :neume="neume.vocalExpressionNeume"
-      :class="[{ red: isRedNeume(neume.vocalExpressionNeume) } ]"></Neume>
+      v-if="hasVocalExpressionNeume && !isVareia(note.vocalExpressionNeume.neume)"
+      :neume="note.vocalExpressionNeume.neume"
+      :class="[{ red: isRedNeume(note.vocalExpressionNeume.neume) } ]"></Neume>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { NoteElement } from '@/models/Element';
-import { QuantitativeNeume, TimeNeume, isHighNeume, isRedNeume, VocalExpressionNeume } from '../models/Neumes';
+import { QuantitativeNeume, TimeNeume, isHighNeume, isRedNeume, VocalExpressionNeume } from '@/models/Neumes';
 import { neumeMap } from '@/models/NeumeMappings';
 import Neume from '@/components/Neume.vue';
+import TimeNeumeBox from '@/components/TimeNeume.vue';
 
 @Component({
   components: {
-    Neume
+    Neume,
+    TimeNeumeBox,
   }
 })
 export default class NeumeBoxSyllable extends Vue {
-  @Prop() neume!: NoteElement;
+  @Prop() note!: NoteElement;
 
   get hasVocalExpressionNeume() {
-    return this.neume.vocalExpressionNeume != null;
+    return this.note.vocalExpressionNeume != null;
   }
 
   get hasTimeNeume() {
-    return this.neume.timeNeume != null;
+    return this.note.timeNeume != null;
   }
 
   get hasFthora() {
-    return this.neume.fthora != null;
-  }
-
-  get quantitativeNeumeMapping() {
-    return neumeMap.get(this.neume.quantitativeNeume!)!;
-  }
-
-  get timeNeumeMapping() {
-    return neumeMap.get(this.neume.timeNeume!)!;
-  }
-
-  get quantitativeNeumeStyle() {
-    const style = {
-      fontFamily: this.quantitativeNeumeMapping.fontFamily
-    } as CSSStyleDeclaration;
-
-    return style;
+    return this.note.fthora != null;
   }
 
   get timeNeumeStyle() {
-    const style = {
-      fontFamily: this.timeNeumeMapping.fontFamily
-    } as CSSStyleDeclaration;
+    const style = {} as CSSStyleDeclaration;
     
-    switch(this.neume.timeNeume) {
+    switch(this.note.timeNeume!.neume) {
       case TimeNeume.Gorgon_Top:
-        if (this.neume.quantitativeNeume == QuantitativeNeume.Apostrophos) {
+        if (this.note.quantitativeNeume.neume === QuantitativeNeume.Apostrophos) {
           style.position = 'relative';
           style.left = '0.6rem';
         }
         break;
       case TimeNeume.Klasma_Top:
-        if (this.neume.quantitativeNeume == QuantitativeNeume.Apostrophos) {
+        if (this.note.quantitativeNeume.neume === QuantitativeNeume.Apostrophos) {
           style.position = 'relative';
           style.left = '0.5rem';
         }
         break;
     }
-  
+    console.log(style);
     return style;
   }
 
@@ -91,7 +75,7 @@ export default class NeumeBoxSyllable extends Vue {
     return isHighNeume(neume);
   }
 
-  isRedNeume(neume: TimeNeume) {
+  isRedNeume(neume: VocalExpressionNeume) {
     return isRedNeume(neume);
   }
 
@@ -106,6 +90,7 @@ export default class NeumeBoxSyllable extends Vue {
 
 .neume {
     font-size: 1.6rem;
+    cursor: default;
 }
 
 .red {

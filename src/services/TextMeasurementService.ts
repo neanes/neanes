106c24880cf9d0da722b12fa691aea57615adfa7
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2021-12-04 16:34:29 -0600

    Move page processing into LayoutService

---
+++ src/services/TextMeasurementService.ts
@@ -0,0 +1,19 @@
+export class TextMeasurementService {
+    private static canvas: HTMLCanvasElement | null = null;
+
+    public static getTextWidth(text: string, font: string) {
+        let canvas = this.canvas || document.createElement("canvas");
+        let context = canvas.getContext("2d")!;
+        context.font = font;
+        let metrics = context.measureText(text);
+        return metrics.width;
+    }
+
+    public static getTextHeight(text: string, font: string) {
+        let canvas = this.canvas || document.createElement("canvas");
+        let context = canvas.getContext("2d")!;
+        context.font = font;
+        let metrics = context.measureText(text);
+        return metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+    }
+}

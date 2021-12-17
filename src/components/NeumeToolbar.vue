13977a8a76f3b5d9e39cd07fc49a8f5fd66ed739
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2021-12-17 17:08:48 -0600

    Move vocal expression neumes to toolbar

--- src/components/NeumeToolbar.vue
+++ src/components/NeumeToolbar.vue
@@ -3,13 +3,19 @@
     <button class="entry-mode-btn" @click="$emit('toggleEntryMode')" :class="{ on: entryMode === true }">Entry Mode</button>
     <button class="neume-button" @click="setAccidental(Accidental.Flat_2_Left)"><Neume class="red neume flat" :neume=" Accidental.Flat_2_Right" /></button>
     <button class="neume-button" @click="setAccidental(Accidental.Sharp_2_Left)"><Neume class="red neume sharp" :neume="Accidental.Sharp_2_Left" /></button>
+    <span class="space"></span>
+    <button class="neume-button" @click="setVocalExpression(VocalExpressionNeume.Vareia)"><Neume class="neume vareia" :neume="VocalExpressionNeume.Vareia" /></button>
+    <button class="neume-button" @click="setVocalExpression(VocalExpressionNeume.Homalon)"><Neume class="neume homalon" :neume="VocalExpressionNeume.Homalon" /></button>
+    <button class="neume-button" @click="setVocalExpression(VocalExpressionNeume.Antikenoma)"><Neume class="neume antikenoma" :neume="VocalExpressionNeume.Antikenoma" /></button>
+    <button class="neume-button" @click="setVocalExpression(VocalExpressionNeume.Psifiston)"><Neume class="neume psifiston" :neume="VocalExpressionNeume.Psifiston" /></button>
+    <button class="neume-button" @click="setVocalExpression(VocalExpressionNeume.Heteron)"><Neume class="red neume heteron" :neume="VocalExpressionNeume.Heteron" /></button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import { AccidentalElement, NoteElement } from '@/models/Element';
-import { Accidental } from '@/models/Neumes';
+import { NoteElement } from '@/models/Element';
+import { Accidental, VocalExpressionNeume } from '@/models/Neumes';
 import Neume from './Neume.vue';
 
 @Component({
@@ -21,13 +27,25 @@ export default class NeumeToolbar extends Vue {
   @Prop() element!: NoteElement;
   @Prop() entryMode!: boolean;
   Accidental = Accidental;
+  VocalExpressionNeume = VocalExpressionNeume;
   
   private setAccidental(neume: Accidental) {
     if (this.element.accidental != null && this.element.accidental.neume === neume) {
-      this.element.accidental = null;
+      this.element.setAccidental(null);
     }
     else {
-      this.element.accidental = new AccidentalElement(neume);
+      this.element.setAccidental(neume);
+    }
+
+    this.$emit('scoreUpdated');
+  }
+
+    private setVocalExpression(neume: VocalExpressionNeume) {
+    if (this.element.vocalExpressionNeume != null && this.element.vocalExpressionNeume.neume === neume) {
+      this.element.setVocalExpressionNeume(null);
+    }
+    else {
+      this.element.setVocalExpressionNeume(neume);
     }
 
     this.$emit('scoreUpdated');
@@ -59,17 +77,49 @@ export default class NeumeToolbar extends Vue {
   .neume-button {
     height: 32px;
     width: 32px;
+
+    position: relative;
+  }
+
+  .space {
+    width: 16px;
   }
 
   .sharp {
     top: -23px;
     left: 24px;
-    position: relative;
   }
 
   .flat {
     top: 2px;
     left: 2px;
-    position: relative;
+  }
+
+  .vareia {
+    top: -10px;
+  }
+
+  .homalon {
+    top: -15px;
+    left: 10px;
+    font-size: 22px;
+  }
+
+  .antikenoma {
+    top: -12px;
+    left: 14px;
+    font-size: 20px;
+  }
+
+  .psifiston {
+    top: -10px;
+    left: 13px;
+    font-size: 18px;
+  }
+
+  .heteron {
+    top: -15px;
+    left: 35px;
+    font-size: 20px;
   }
   </style>

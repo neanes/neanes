64c2b3206d3f15a8f43546b9f052dd376b6a6856
Daniel Godber <danielgarthur@gmail.com>
2021-12-26 18:04:57 -0600

    Add auto-update

--- src/background.ts
+++ src/background.ts
@@ -9,6 +9,7 @@ import {
   ipcMain,
   MenuItemConstructorOptions,
 } from 'electron';
+import { autoUpdater } from 'electron-updater';
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib';
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer';
 import {
@@ -341,6 +342,7 @@ async function createWindow() {
     createProtocol('app');
     // Load the index.html when not in development
     win.loadURL('app://./index.html');
+    autoUpdater.checkForUpdatesAndNotify();
   }
 }
 

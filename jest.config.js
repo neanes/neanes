dbf3e703227710d659fc2d868476753020261ba8
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2022-01-04 20:37:27 -0600

    Add tests

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+};

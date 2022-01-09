d3ab4769e30a1553fd21e6f949bcbe42afa2390e
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2022-01-09 17:27:31 -0600

    Update lifecycle event for dialogs

--- src/components/ModeKeyDialog.vue
+++ src/components/ModeKeyDialog.vue
@@ -105,7 +105,7 @@ export default class ModeKeyDialog extends Vue {
     window.addEventListener('keydown', this.onKeyDown);
   }
 
-  destroyed() {
+  beforeDestroy() {
     window.removeEventListener('keydown', this.onKeyDown);
   }
 

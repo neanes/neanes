d4ec19e7a11d2e6ccf3ebe56d0e651ec16e9d56e
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2022-05-14 12:56:37 -0500

    Add Docs (#7)
    Adds a VuePress website containing documentation and download links.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: 'Neanes Byzantine Notation Scorewriter',
+  description:
+    'Neanes is a free and open source scorewriter for notating Byzantine chant in Byzantine notation.',
+  base: '/neanes/',
+  head: [['link', { rel: 'shortcut icon', href: '/favicon.ico' }]],
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      {
+        text: 'Download',
+        link: '/download/',
+      },
+      {
+        text: 'Changelog',
+        link: 'https://github.com/danielgarthur/neanes/releases',
+      },
+    ],
+    sidebar: {
+      '/guide/': [''],
+      '/download/': [''],
+    },
+    repo: 'danielgarthur/neanes',
+    docsDir: 'docs',
+    //editLinks: true,
+    //editLinkText: 'Is something wrong or missing? Edit this page on github!',
+  },
+};

726fe8b63ee992523c575cff97fd29202d62d815
Basil Crow <me@basilcrow.com>
2023-06-21 15:47:23 -0700

    Migrate to an external event emitter library

--- src/eventBus.ts
+++ src/eventBus.ts
@@ -1,2 +1,9 @@
-import Vue from 'vue';
-export const EventBus = new Vue();
+import mitt from 'mitt';
+const emitter = mitt();
+export const EventBus = {
+  $on: (eventName: string, func: (...args: any[]) => unknown) =>
+    emitter.on(eventName, func),
+  $off: (eventName: string, func: (...args: any[]) => unknown) =>
+    emitter.off(eventName, func),
+  $emit: (type: string, event?: unknown) => emitter.emit(type, event),
+};

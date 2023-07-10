e0a7723729fd6ddf78257f6bb3a3cb2528f57517
Basil Crow <me@basilcrow.com>
2023-07-10 10:23:04 -0700

    Restore PWA functionality to web application (#156)

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { rmSync } from 'node:fs';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import electron from 'vite-plugin-electron';
@@ -21,6 +22,71 @@ export default defineConfig(({ command, mode }) => {
       },
     },
     plugins: [
+      VitePWA({
+        includeAssets: [
+          'favicon-32.png',
+          'favicon-16.png',
+          'apple-touch-icon.png',
+          'msapplication-icon.png',
+          'safari-pinned-tab.svg',
+        ],
+        manifest: {
+          name: 'Neanes',
+          short_name: 'Neanes',
+          description: 'A Byzantine Chant Scorewriter',
+          theme_color: '#052F43',
+          icons: [
+            {
+              src: 'assets/icons/icon-72x72.png',
+              sizes: '72x72',
+              type: 'image/png',
+              purpose: 'maskable any',
+            },
+            {
+              src: 'assets/icons/icon-96x96.png',
+              sizes: '96x96',
+              type: 'image/png',
+              purpose: 'maskable any',
+            },
+            {
+              src: 'assets/icons/icon-128x128.png',
+              sizes: '128x128',
+              type: 'image/png',
+              purpose: 'maskable any',
+            },
+            {
+              src: 'assets/icons/icon-144x144.png',
+              sizes: '144x144',
+              type: 'image/png',
+              purpose: 'maskable any',
+            },
+            {
+              src: 'assets/icons/icon-152x152.png',
+              sizes: '152x152',
+              type: 'image/png',
+              purpose: 'maskable any',
+            },
+            {
+              src: 'assets/icons/icon-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'maskable any',
+            },
+            {
+              src: 'assets/icons/icon-384x384.png',
+              sizes: '384x384',
+              type: 'image/png',
+              purpose: 'maskable any',
+            },
+            {
+              src: 'assets/icons/icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable any',
+            },
+          ],
+        },
+      }),
       vue(),
       eslintPlugin(),
       !mode.includes('web')

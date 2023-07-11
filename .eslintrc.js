52261d9507e6518db22dbc9c7e46eb348db8867e
Basil Crow <me@basilcrow.com>
2023-07-10 21:30:28 -0700

    Enable `eslint-config-prettier`

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,9 @@ module.exports = {
     node: true,
   },
   extends: [
-    'eslint:recommended',
     'plugin:vue/vue3-essential',
-    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
   ],
   rules: {
     'no-console':
@@ -14,8 +14,9 @@ module.exports = {
         ? ['error', { allow: ['warn', 'error'] }]
         : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',

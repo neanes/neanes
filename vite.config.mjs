275b8ec060d67e441e576353ed7390bb8f5bbcc7
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2023-11-22 12:34:35 -0600

    chore: upgrade to vite 5

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,12 @@
 import { rmSync } from 'node:fs';
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
-import path from 'path';
+
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig } from 'vite';
 import electron from 'vite-plugin-electron';
 import eslintPlugin from 'vite-plugin-eslint';
+import { VitePWA } from 'vite-plugin-pwa';
+
 import pkg from './package.json';
 
 // https://vitejs.dev/config/

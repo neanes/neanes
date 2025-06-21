neanes/neanes

commit 152892fb1443dde4ae8367bf0cd47c39257b1002

Add common combinations to the neume selector (#1517)

src/components/Editor.vue
@@ -34,12 +34,30 @@
       @open-playback-settings="openPlaybackSettingsDialog"
     />
     <div class="content">
-      <NeumeSelector
-        class="neume-selector"
-        :pageSetup="score.pageSetup"
-        :neumeKeyboard="neumeKeyboard"
-        @select-quantitative-neume="addQuantitativeNeume"
-      />
+      <div class="left-panel">
+        <NeumeSelector
+          class="neume-selector"
+          :pageSetup="score.pageSetup"
+          :neumeKeyboard="neumeKeyboard"
+          @select-quantitative-neume="addQuantitativeNeume"
+        />
+        <div
+          class="neume-combo-header"
+          @click="neumeComboPanelIsExpanded = !neumeComboPanelIsExpanded"
+        >
+          {{ $t('editor:common.neumeComboHeader') }}
+          <span class="neume-combo-expand-collapse">{{
+            neumeComboPanelIsExpanded ? '\u2796' : '\u2795'
+          }}</span>
+        </div>
+        <NeumeComboSelector
+          v-if="neumeComboPanelIsExpanded"
+          class="neume-combo-selector"
+          :pageSetup="score.pageSetup"
+          @select-neume-combo="addNeumeCombination"
+        />
+      </div>
+
       <div class="page-container">
         <Vue3TabsChrome
           class="workspace-tab-container"
@@ -1257,6 +1275,7 @@ import EmptyNeumeBox from '@/components/NeumeBoxEmpty.vue';
 import MartyriaNeumeBox from '@/components/NeumeBoxMartyria.vue';
 import SyllableNeumeBox from '@/components/NeumeBoxSyllable.vue';
 import TempoNeumeBox from '@/components/NeumeBoxTempo.vue';
+import NeumeComboSelector from '@/components/NeumeComboSelector.vue';
 import NeumeSelector from '@/components/NeumeSelector.vue';
 import PageSetupDialog from '@/components/PageSetupDialog.vue';
 import PlaybackSettingsDialog from '@/components/PlaybackSettingsDialog.vue';
@@ -1308,6 +1327,7 @@ import { EntryMode } from '@/models/EntryMode';
 import { Footer } from '@/models/Footer';
 import { Header } from '@/models/Header';
 import { modeKeyTemplates } from '@/models/ModeKeys';
+import { NeumeCombination } from '@/models/NeumeCommonCombinations';
 import {
   areVocalExpressionsEquivalent,
   getSecondaryNeume,
@@ -1384,6 +1404,7 @@ interface Vue3TabsChromeComponent {
     MartyriaNeumeBox,
     TempoNeumeBox,
     EmptyNeumeBox,
+    NeumeComboSelector,
     NeumeSelector,
     ContentEditable,
     TextBox,
@@ -1468,6 +1489,8 @@ export default class Editor extends Vue {
   pageSetupDialogIsOpen: boolean = false;
   editorPreferencesDialogIsOpen: boolean = false;
   exportDialogIsOpen: boolean = false;
+
+  neumeComboPanelIsExpanded: boolean = false;
 
   exportFormat: ExportFormat = ExportFormat.PNG;
 
@@ -2773,6 +2796,14 @@ export default class Editor extends Vue {
     }
 
     this.save();
+  }
+
+  addNeumeCombination(combo: NeumeCombination) {
+    const backup = this.clipboard.slice();
+    this.clipboard = combo.elements;
+    this.onPasteScoreElements(false);
+
+    this.clipboard = backup;
   }
 
   addAutoMartyria(alignRight?: boolean, note?: Note) {
@@ -7615,8 +7646,37 @@ export default class Editor extends Vue {
   overflow: auto;
 }
 
+.left-panel {
+  display: flex;
+  flex-direction: column;
+}
+
 .neume-selector {
+  flex: 2;
   overflow: auto;
+}
+
+.neume-combo-selector {
+  flex: 1;
+  overflow: auto;
+}
+
+.neume-combo-header {
+  display: flex;
+  justify-content: center;
+  cursor: default;
+  user-select: none;
+  padding: 0.5rem 0.25rem;
+  background-color: #eee;
+}
+
+.neume-combo-header:hover {
+  background-color: #ddd;
+}
+
+.neume-combo-expand-collapse {
+  margin-left: auto;
+  margin-right: 0.25rem;
 }
 
 .mode-header {

src/components/NeumeComboSelector.vue
@@ -1,0 +1,61 @@
+<template>
+  <div class="neume-combo-selector-panel">
+    <div
+      class="row"
+      v-for="(combo, index) in combos"
+      :key="index"
+      @click="$emit('select-neume-combo', combo)"
+    >
+      <SyllableNeumeBox
+        v-for="(neume, index) in combo.elements"
+        :key="index"
+        :note="neume"
+        :pageSetup="pageSetup"
+        class="neume"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-facing-decorator';
+
+import SyllableNeumeBox from '@/components/NeumeBoxSyllable.vue';
+import {
+  NeumeCombination,
+  NeumeCommonCombinations,
+} from '@/models/NeumeCommonCombinations';
+import { PageSetup } from '@/models/PageSetup';
+
+@Component({
+  components: { SyllableNeumeBox },
+  emits: ['select-neume-combo'],
+})
+export default class NeumeComboSelector extends Vue {
+  @Prop() pageSetup!: PageSetup;
+
+  combos: NeumeCombination[] = [
+    NeumeCommonCombinations.ending1,
+    NeumeCommonCombinations.ending2,
+    NeumeCommonCombinations.ornament1,
+    NeumeCommonCombinations.ornament1Alt,
+  ];
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.row {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.neume {
+  margin: 0 0.125rem;
+}
+
+.row:hover {
+  background-color: aliceblue;
+}
+</style>

src/i18n/el/index.ts
@@ -1,10 +1,12 @@
 import dialog from './dialog.json';
+import editor from './editor.json';
 import menu from './menu.json';
 import model from './model.json';
 import toolbar from './toolbar.json';
 
 const resources = {
   dialog,
+  editor,
   menu,
   model,
   toolbar,

src/i18n/en/index.ts
@@ -1,10 +1,12 @@
 import dialog from './dialog.json';
+import editor from './editor.json';
 import menu from './menu.json';
 import model from './model.json';
 import toolbar from './toolbar.json';
 
 const resources = {
   dialog,
+  editor,
   menu,
   model,
   toolbar,

src/i18n/ro/index.ts
@@ -1,10 +1,12 @@
 import dialog from './dialog.json';
+import editor from './editor.json';
 import menu from './menu.json';
 import model from './model.json';
 import toolbar from './toolbar.json';
 
 const resources = {
   dialog,
+  editor,
   menu,
   model,
   toolbar,

src/models/NeumeCommonCombinations.ts
@@ -1,0 +1,130 @@
+import { NoteElement } from './Element';
+import {
+  GorgonNeume,
+  QuantitativeNeume,
+  TimeNeume,
+  VocalExpressionNeume,
+} from './Neumes';
+
+export interface NeumeCombination {
+  name: string;
+  tags: string[];
+  description?: string;
+
+  elements: NoteElement[];
+}
+
+const ending1: NeumeCombination = {
+  name: 'Ending 1',
+  tags: ['ending'],
+  elements: [
+    createNote({
+      vareia: true,
+      quantitativeNeume: QuantitativeNeume.Apostrophos,
+      timeNeume: TimeNeume.Klasma_Top,
+    }),
+    createNote({
+      quantitativeNeume: QuantitativeNeume.Hyporoe,
+      gorgonNeume: GorgonNeume.Gorgon_Top,
+    }),
+    createNote({
+      quantitativeNeume: QuantitativeNeume.Oligon,
+      gorgonNeume: GorgonNeume.Gorgon_Top,
+      vocalExpressionNeume: VocalExpressionNeume.Antikenoma,
+    }),
+    createNote({
+      quantitativeNeume: QuantitativeNeume.Apostrophos,
+    }),
+    createNote({
+      quantitativeNeume: QuantitativeNeume.Ison,
+      timeNeume: TimeNeume.Klasma_Top,
+    }),
+  ],
+};
+
+const ending2: NeumeCombination = {
+  name: 'Ending 2',
+  tags: ['ending'],
+  elements: [
+    createNote({
+      vareia: true,
+      quantitativeNeume: QuantitativeNeume.Ison,
+      timeNeume: TimeNeume.Klasma_Top,
+    }),
+    createNote({
+      quantitativeNeume: QuantitativeNeume.Hyporoe,
+      gorgonNeume: GorgonNeume.Gorgon_Top,
+    }),
+    createNote({
+      quantitativeNeume: QuantitativeNeume.Oligon,
+      gorgonNeume: GorgonNeume.Gorgon_Top,
+      vocalExpressionNeume: VocalExpressionNeume.Antikenoma,
+    }),
+    createNote({
+      quantitativeNeume: QuantitativeNeume.Apostrophos,
+    }),
+    createNote({
+      quantitativeNeume: QuantitativeNeume.Ison,
+      timeNeume: TimeNeume.Klasma_Top,
+    }),
+  ],
+};
+
+const ornament1: NeumeCombination = {
+  name: 'Ornament 1',
+  tags: ['ornament'],
+  elements: [
+    createNote({
+      quantitativeNeume: QuantitativeNeume.OligonPlusIsonPlusKentemata,
+      gorgonNeume: GorgonNeume.Gorgon_Top,
+    }),
+    createNote({
+      vareia: true,
+      quantitativeNeume: QuantitativeNeume.Ison,
+    }),
+    createNote({
+      quantitativeNeume: QuantitativeNeume.Apostrophos,
+      gorgonNeume: GorgonNeume.Gorgon_Top,
+    }),
+    createNote({
+      quantitativeNeume: QuantitativeNeume.Ison,
+      timeNeume: TimeNeume.Klasma_Top,
+    }),
+  ],
+};
+
+const ornament1Alt: NeumeCombination = {
+  name: 'Ornament 1 (alt)',
+  tags: ['ornament'],
+  elements: [
+    createNote({
+      quantitativeNeume: QuantitativeNeume.OligonPlusIsonPlusKentemata,
+      gorgonNeume: GorgonNeume.GorgonDottedRight,
+    }),
+    createNote({
+      vareia: true,
+      quantitativeNeume: QuantitativeNeume.Ison,
+    }),
+    createNote({
+      quantitativeNeume: QuantitativeNeume.Apostrophos,
+      gorgonNeume: GorgonNeume.Gorgon_Top,
+    }),
+    createNote({
+      quantitativeNeume: QuantitativeNeume.Ison,
+      timeNeume: TimeNeume.Klasma_Top,
+    }),
+  ],
+};
+
+function createNote(args: Partial<NoteElement>): NoteElement {
+  const note = new NoteElement();
+  Object.assign(note, args);
+  return note;
+}
+
+export const NeumeCommonCombinations = {
+  ending1,
+  ending2,
+  ornament1,
+  ornament1Alt,
+};

commit 45e4e76d7f22811795436bedfa1ea925d44e09a4

Fix print issue introduced in #1517

src/components/Editor.vue
@@ -7886,7 +7886,7 @@ export default class Editor extends Vue {
   }
 
   .file-menu-bar,
-  .neume-selector-panel,
+  .left-panel,
   .workspace-tab-container,
   .lyrics-toolbar,
   .lyric-manager-toolbar,
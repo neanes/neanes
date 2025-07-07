b3016fa10a3b77c66d6ca0ad5243ee36b2262c47
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2025-07-06 22:12:15 -0500

    Rework the playback service (#1539)
    * WIP messing around with upheavals
    
    * More updates
    
    * Update test
    
    * Update tests
    
    * Fix bug
    
    * More messing around
    
    * More messing around
    
    * Clean up unused flag
    
    * Use actual diatonic scale for repositioning
    
    * Reset pitch at mode key
    
    * Fix mistake in last commit
    
    * Add failsafe for invalid frequencies
    
    * Clean up log messages
    
    * Remove unused method
    
    * Update test

--- src/env.d.ts
+++ src/env.d.ts
@@ -9,6 +9,8 @@ interface ImportMetaEnv {
   readonly VITE_DOWNLOAD_URL: string;
   readonly VITE_BYZHTML_VERSION: string;
   readonly VITE_IS_ELECTRON: string;
+  readonly VITE_PLAYBACK_SERVICE_LOGGING_ENABLED?: string;
+  readonly VITE_AUDIO_SERVICE_LOGGING_ENABLED?: string;
 }
 
 interface ImportMeta {

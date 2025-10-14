ab8d8923c0bd799fd6748b1512c21a95e81ccf20
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2025-10-14 16:38:44 -0500

    Remove unused file

--- src/components/Editor/Vue3TabsChromeComponent.ts
+++
@@ -1,6 +0,0 @@
-import { Tab } from 'vue3-tabs-chrome';
-
-export interface Vue3TabsChromeComponent {
-  addTab: (...newTabs: Array<Tab>) => void;
-  removeTab: (tabKey: string | number) => void;
-}

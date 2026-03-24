2b03b30bd665ce15e10aa4e85ecd5bf8e9e7194f
dedysutanto <dedysutanto@gmail.com>
2026-03-24 11:10:39 +0700

    Add folder for Bahasa Indonesia translation

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,10 +1,12 @@
 import el from './el';
 import en from './en';
 import ro from './ro';
+import id from './id';
 
 export const defaultNS = 'model';
 export const resources = {
   el,
   en,
   ro,
+  id,
 } as const;

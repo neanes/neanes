c7c674b757784721381914d3e55b44b35d11d321
Basil Crow <me@basilcrow.com>
2026-06-05 11:59:54 -0700

    Remove dead vue-cli-plugin-electron-builder code

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1711,11 +1711,6 @@ async function createWindow() {
     minWidth,
     minHeight,
     webPreferences: {
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env
-        .ELECTRON_NODE_INTEGRATION as unknown as boolean,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
       preload,
       spellcheck: false,
     },

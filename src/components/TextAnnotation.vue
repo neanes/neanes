efc62b6d5d62a42a1a06d883b98d46bde4e0a185
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2026-06-12 15:24:12 -0500

    Make `TextAnnotation` and `TextBoxRich` use the same fallback font policy for inserted CKEditor neumes.

--- src/components/TextAnnotation.vue
+++ src/components/TextAnnotation.vue
@@ -69,7 +69,7 @@ const style = computed(() => {
     top: withZoom(elementY.value),
     '--ck-content-font-family': getFontFamilyWithFallback(
       props.pageSetup.textBoxDefaultFontFamily,
-      props.pageSetup.neumeDefaultFontFamily,
+      props.pageSetup.neumeDefaultFontFamily + 'Legacy', // TODO what a terrible hack
     ),
     '--ck-content-font-size': `${props.pageSetup.lyricsDefaultFontSize}px`, // no zoom because we will apply zooming on the whole editor
     '--ck-content-line-height': 'normal',
